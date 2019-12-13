6325e519fae31df74f40b233d75c2d0f540f7235
fgomesc <fgomesc0586@gmail.com>
2019-12-13 16:19:40 -0300

    Reset Passqord

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,14 +1,21 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
-from simplemooc.accounts.views import register, dashboard, edit, edit_password
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import path, include
+from simplemooc.accounts.views import register, dashboard, edit, edit_password, password_reset
+from django.contrib.auth import views as auth_views
 
-app_name='accounts'
+app_name = 'accounts'
 
 urlpatterns = [
-    path('', dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('painel/', dashboard, name='dashboard'),
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('sair/', LogoutView.as_view(next_page='home'), name='logout'),
     path('cadastro/', register, name='register'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),name='password_reset_complete'),
     path('editar/', edit, name='edit'),
-    path('editar-senha/', edit_password, name='edit_password')
+    path('editar-senha/', edit_password, name='edit_password'),
 ]
--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -127,13 +127,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
 
 # E-mails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'email@gmail.com'
-EMAIL_HOST_PASSWORD = 'senha'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 CONTACT_EMAIL = 'contato@simplemooc.com'
 
